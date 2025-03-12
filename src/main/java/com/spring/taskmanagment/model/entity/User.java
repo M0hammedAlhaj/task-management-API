7934ea7f8ebb8f885b468c29700edd226f39a651
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-03-12 04:55:07 +0300

    Add Constraint For User Class

--- src/main/java/com/spring/taskmanagment/model/entity/User.java
+++ src/main/java/com/spring/taskmanagment/model/entity/User.java
@@ -3,8 +3,10 @@ package com.spring.taskmanagment.model.entity;
 import com.spring.taskmanagment.model.TimeLogType;
 import jakarta.annotation.PreDestroy;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -12,14 +14,9 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Table(name = "users",
-        uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 
 public class User implements Serializable {
-
-    @Serial
-    private static final long serialVersionUID = -4033047012569507069L; // or use a custom number
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,42 +24,35 @@ public class User implements Serializable {
     private Long userId;
 
     @Column(name = "user_name")
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String userName;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 
 
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-            name = "user_role",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
 
-    @OneToMany(mappedBy = "userAuthor",
-            cascade = CascadeType.REMOVE,
-            fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "userAuthor", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
     private Set<Comment> comments = new HashSet<>();
 
-    @OneToMany(mappedBy = "userOwner",
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "userOwner", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Task> taskOwned = new HashSet<>();
 
     @OneToMany(mappedBy = "userAssign")
     private Set<Task> taskAssigned = new HashSet<>();
 
-    @ManyToMany(fetch = FetchType.LAZY,
-            cascade = CascadeType.PERSIST)
-    @JoinTable(
-            name = "time_log_user",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "time_log_id")
-    )
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "time_log_user", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "time_log_id"))
     private Set<TimeLog> timeLogs = new HashSet<>();
 
 
