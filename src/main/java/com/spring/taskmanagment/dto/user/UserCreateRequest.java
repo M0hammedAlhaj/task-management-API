bcac8c217059fb96df53ee8f18cc60f2c6be4b6a
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-03-12 04:57:46 +0300

    Add Constraint For UserResponse Class

--- src/main/java/com/spring/taskmanagment/dto/user/UserCreateRequest.java
+++ src/main/java/com/spring/taskmanagment/dto/user/UserCreateRequest.java
@@ -1,16 +1,24 @@
 package com.spring.taskmanagment.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 
 public class UserCreateRequest implements Serializable {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String userName;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 
-    @NotNull
+    @NotNull(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
     public String getUserName() {
